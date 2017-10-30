817ebb71834f1ad70ab8996b55a0d0caad93d04f
ricardosalinase <ricardo.salinase@gmail.com>
2017-10-30 13:53:31 -0300

    gitignore

---
+++ foodutils.py
@@ -0,0 +1,11 @@
+
+def greet_dude_with_food(dude_name, food_today):
+    """
+    funciond de prueba hecha por Ricardo Salinas
+        :param dude_name: 
+        :param food_today:
+        :return message
+    """
+    return "Hey {dude_name}! Want a {food_today} today?".format(
+        dude_name=dude_name,
+        food_today=food_today)
